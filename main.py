ae4379334632c47401f47f33ee19c8222d47540f
hunter <watermelonmcmuffintv@gmail.com>
2022-10-24 21:13:24 -0400

    added reqs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 from fztk.Button import FzTkButton
@@ -15,27 +17,32 @@ pygame.display.set_caption("Faction: Zero")
 
 # create FzEngine objects
 font_handler = FzTkFontHandler()
-
 clock = pygame.time.Clock()
 
-# create button
-button = FzTkButton(
-    font_handler=font_handler,
-    x=int(WIDTH - 180),
-    y=0,
-    height=60,
-    width=180,
-    surface=screen,
-    text="Toggle Scene",
-    tooltip="Changes between development engine scenes."
-)
 
 #button.register_click_callback(lambda: scene_handler.dev_toggle_scene())
 
-while True:
-    delta_time = clock.tick(60) / 1000
+if __name__ == "__main__":
+    print("Getting screen sizes")
+    WIDTH = int(os.system("cat /sys/class/graphics/fb0/virtual_size | cut -d, -f1"))
+    HEIGHT = int(os.system("cat /sys/class/graphics/fb0/virtual_size | cut -d, -f2"))
 
-    button.set_text("ass")
-    button.update()
+    # create button
+    button = FzTkButton(
+        font_handler=font_handler,
+        x=int(WIDTH - 180),
+        y=0,
+        height=60,
+        width=180,
+        surface=screen,
+        text="Toggle Scene",
+        tooltip="Changes between development engine scenes."
+    )
 
-    pygame.display.update()
+    while True:
+        delta_time = clock.tick(60) / 1000
+
+        button.set_text("ass")
+        button.update()
+
+        pygame.display.update()
